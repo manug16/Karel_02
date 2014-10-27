fae6af3c6318bdee68beea6d9895438399fec24f
manug16 <manu@ManuMacBook.local>
2014-10-27 18:02:05 +0000

    Lab02 Manu Gualandri
    Ready to hand in

--- Lab02.java
+++ Lab02.java
@@ -1,9 +1,12 @@
 /**
 *
-* Description of the program goes here  // provide a brief description
+* Description of the program goes here Create Unit1\Lab02.java with “maze” map at size 8x8.  
+Instantiate one athlete object and direct it to escape the maze.  
+Leave a trail of beepers to mark the path.  
+By default, athletes begin with an infinite number of beepers. // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Manu Gualandri>  // replace <...> with your name
+* @version <27/10/2014> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
@@ -12,6 +15,48 @@ import edu.fcps.karel2.Robot;
 public class Lab02 {
 
      public static void main(String[] args) {
-          // TODO Your code goes here
+     Display.openWorld("maps/maze.map");
+     Display.setSize(8, 8);
+     Display.setSpeed(3);
+     Athlete manu = new Athlete();
+     manu.putBeeper();
+     manu.move();
+     manu.putBeeper();
+     manu.turnRight();
+     manu.move();
+     manu.putBeeper();
+     manu.turnRight();
+     manu.move();
+     manu.putBeeper();
+     manu.turnLeft();
+     manu.move();
+     manu.putBeeper();
+     manu.turnLeft();
+     manu.move();
+     manu.putBeeper();
+     manu.turnRight();
+     manu.move();
+     manu.putBeeper();
+     manu.move();
+     manu.putBeeper();
+     manu.turnRight();
+     manu.move();
+     manu.putBeeper();
+     manu.turnLeft();
+     manu.move();
+     manu.putBeeper();
+     manu.turnLeft();
+     manu.move();
+     manu.putBeeper();
+     manu.move();
+     manu.putBeeper();
+     manu.move();
+     manu.putBeeper();
+     manu.move();
+     manu.putBeeper();
+     manu.turnRight();
+     manu.move();
+     manu.putBeeper();
+     manu.move();
      }
 }
